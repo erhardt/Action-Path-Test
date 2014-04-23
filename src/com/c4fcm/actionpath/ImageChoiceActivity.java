19acee7448fdb94d62a3541a9c22ceb8304d784e
Nathan Matias <natematias@gmail.com>
2014-04-22 20:40:06 -0400

    added survey Thanks page

--- src/com/c4fcm/actionpath/ImageChoiceActivity.java
+++ src/com/c4fcm/actionpath/ImageChoiceActivity.java
@@ -1,8 +1,11 @@
 package com.c4fcm.actionpath;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -22,8 +25,8 @@ public class ImageChoiceActivity extends FragmentActivity {
 	    	images[i].setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View arg0) {
-				   Toast.makeText(ImageChoiceActivity.this,
-					"ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+					Intent i = new Intent(arg0.getContext(), SurveyThanksActivity.class);
+					startActivity(i); 
 				}
 			});
 	    }
@@ -36,4 +39,12 @@ public class ImageChoiceActivity extends FragmentActivity {
     			  getSystemService(NOTIFICATION_SERVICE);
     	notificationManager.cancel(0);
 	}
+	
+	  //creates a PendingIntent
+    public PendingIntent getPendingIntent() {
+    	Log.v("INTENT","returning an intent for ImageChoiceActivity.class");
+    	return PendingIntent.getActivity(this, 0, new Intent(this,
+    			SurveyThanksActivity.class), 0);
+    }
+
 }
---
+++ src/com/c4fcm/actionpath/SurveyThanksActivity.java
@@ -0,0 +1,16 @@
+package com.c4fcm.actionpath;
+
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class SurveyThanksActivity extends FragmentActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_survey_thanks);
+	}
+}
